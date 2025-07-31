69caab507e55c140b713be96430361c242cf1fbb
BenjaUrbin <benjamin.urbina@userena.cl>
2025-07-31 16:41:20 -0400

    Initial commit

---
+++ backend/phoenix/src/test/java/cl/phoenix/phoenix/PhoenixApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.phoenix.phoenix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoenixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
